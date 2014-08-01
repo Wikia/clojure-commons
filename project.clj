48e5a91f7fba97f04046e6cffb13815d976495a4
nmonterroso <nelson@wikia-inc.com>
2014-08-01 15:13:32 -0700

    updating project properties

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clojure-commons "0.1.0-SNAPSHOT"
   :description "Set of common utilities that are useful throughout Wikia's projects"
+  :url "https://github.com/Wikia/clojure-commons"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [environ "0.5.0"]
                  [org.clojure/data.json "0.2.5"]
