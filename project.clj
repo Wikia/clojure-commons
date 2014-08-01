42a1754f130426a749bc20d1078076e3193f6ecf
nmonterroso <nelson@wikia-inc.com>
2014-08-01 15:44:01 -0700

    fixing spacing

--- project.clj
+++ project.clj
@@ -11,4 +11,4 @@
                  [log4j/log4j "1.2.16" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
                  [org.slf4j/slf4j-log4j12 "1.6.4"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [ clj-logging-config "1.9.7"]])
+                 [clj-logging-config "1.9.7"]])
