bb98c2e28d3254603d3e884541eb5ae528dae263
nmonterroso <nelson@wikia-inc.com>
2014-12-01 16:50:45 -0800

    basic clojure -> influxdb over udp

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
-(defproject wikia/commons "0.1.0-SNAPSHOT"
+(defproject wikia/commons "0.1.1-SNAPSHOT"
   :description "Set of common utilities that are useful throughout Wikia's projects"
   :url "https://github.com/Wikia/clojure-commons"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[cheshire "5.3.1"]
+                 [clj-logging-config "1.9.7"]
                  [environ "0.5.0"]
-                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/clojure "1.6.0"]
+
 
                  ; logger
                  [log4j/log4j "1.2.16" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
-                 [org.slf4j/slf4j-log4j12 "1.6.4"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [clj-logging-config "1.9.7"]])
+                 [org.slf4j/slf4j-log4j12 "1.6.4"]])
